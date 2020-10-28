70a99454f3d9afef3fd5d3debb50c32f1321e898
Leo-SIK <05rt06@gmail.com>
2020-10-28 17:37:25 -0400

    Fixed errors

--- sql/Freespacequeries.sql
+++ sql/Freespacequeries.sql
@@ -1,66 +1,66 @@
 -- List of locations shown in the app
 CREATE TABLE Locations (
-IdNumber int SERIAL PRIMARY KEY,
+IdNumber int PRIMARY KEY,
 LocationName varchar(40),
 MaxCapacity int);
 
-
 -- List of current status reports made by users
 CREATE TABLE CurrentStatus (
-ActivityStatus varchar(15) PRIMARY KEY,
+IdNumber int PRIMARY KEY,
+ActivityStatus varchar(15),
 LocationID int references Locations(IdNumber),
 ReportedTime TIMESTAMP);
 
-
 -- List of actual card swipe number reports from dining halls
 CREATE TABLE CurrentPopulation (
--- We will come up with an algorithm to calculate estimated number of people currently at the dining hall
+IdNumber int PRIMARY KEY,
 LocationID int,
+-- We will come up with an algorithm to calculate estimated number of people currently at the dining hall
 EstimatedPopulation int,
 ReportedTime TIME,
 ReportedDay varchar(10),
 CardSwipeNumber int);
 
-
+-- List of users registered to system
 CREATE TABLE Users (
-UserId varchar(25),
+UserId varchar(25) PRIMARY KEY,
 UserPassword varchar(50),
-UserType varchar(20),
-PRIMARY KEY (UserID, UserType));
+UserType varchar(20));
 
-    
-INSERT INTO Locations (LocationName, MaxCapacity)
+
+-- Sample data to populate the tables
+INSERT INTO Locations (IdNumber, LocationName, MaxCapacity)
 VALUES
-('Commons Dining Hall', 500),
-('Knollcrest Dining Hall', 400),
-('Uppercrust', 100);
+(1, 'Commons Dining Hall', 500),
+(2, 'Knollcrest Dining Hall', 400),
+(3, 'Uppercrust', 100);
 
 INSERT INTO Users (
     UserId, UserPassword, UserType) 
 VALUES 
 ('Id_1', 'Password_1', 'Dining Hall Faculty'),
 ('Id_2', 'Password_2', 'Dining Hall Faculty'),
-('UserId_1', 'Password_3', 'User');
+('UserId_1', 'Password_3', 'Calvin User');
 
 
 INSERT INTO CurrentStatus(
-    Status, LocationID, ReportedTime )
+    IdNumber, ActivityStatus, LocationID, ReportedTime )
 VALUES
-    ('Busy',  1, '2020-10-16 13:33:33'),
-    ('Normal', 2, '2020-10-16 14:22:22'),
-    ('Very Busy', 3, '2020-10-16 15:11:11');
-
+    (1, 'Busy',  1, '2020-10-16 13:33:33'),
+    (2, 'Normal', 2, '2020-10-16 14:22:22'),
+    (3, 'Very Busy', 3, '2020-10-16 15:11:11'),
+    (4, 'Busy',  1, '2020-10-16 13:40:33');
 
 
 INSERT INTO CurrentPopulation(
-    EstimatedPopulation, LocationId, ReportedTime, ReportedDay, CardSwipeNumber )
+    IdNumber, LocationId, EstimatedPopulation, ReportedTime, ReportedDay, CardSwipeNumber )
 VALUES
-    (1, 200, '13:30:00', 'Monday', 100),
-    (1, 150, '13:45:00', 'Monday', 50),
-    (2, 150,  '13:30:00', 'Monday', 60),
-    (2, 160,  '13:45:00', 'Monday', 100),
-    (3, 140,  '13:30:00', 'Monday', 80),
-    (3, 200,'13:45:00', 'Monday', 120);
+    (1, 1, 200, '13:30:00', 'Monday', 100),
+    (2, 1, 150, '13:45:00', 'Monday', 50),
+    (3, 2, 150,  '13:30:00', 'Monday', 60),
+    (4, 2, 160,  '13:45:00', 'Monday', 100),
+    (5, 3, 140,  '13:30:00', 'Monday', 80),
+    (6, 3, 200,'13:45:00', 'Monday', 120);
 
 
 
