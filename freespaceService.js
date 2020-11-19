acbb548b8eaaf5aab4ed9aae5c9346856a0bb7dd
Leo-SIK <05rt06@gmail.com>
2020-11-19 16:38:09 -0500

    test

--- freespaceService.js
+++ freespaceService.js
@@ -7,19 +7,6 @@ const db = pgp({
     password: process.env.PASSWORD
 });
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
 
 // Configure the server and its routes.
 
@@ -33,17 +20,10 @@ router.get("/", readHelloMessage);
 router.get("/curentpopulation", readCurrentPopulation);
 router.get("/users", readUsers);
 
-app.configure(function () {
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(allowCrossDomain);
-    app.use(express.static(path.join(application_root, "public")));
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-    app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(router);
+app.use(errorHandler);
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
-  });
-  
 // Implement the CRUD operations.
 
 function errorHandler(err, req, res) {
