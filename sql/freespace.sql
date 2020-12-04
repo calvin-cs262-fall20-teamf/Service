236fcc545b4873ab37adb92da3dc0a3108b7402c
ZC <zc@ZCs-MacBook-Air.local>
2020-12-04 00:31:00 -0700

    Updated method to join tables more easily

--- sql/freespace.sql
+++ sql/freespace.sql
@@ -5,53 +5,44 @@
 --
 
 -- Drop previous versions of the tables if they they exist, in reverse order of foreign keys.
-DROP TABLE IF EXISTS CurrentStatus;
+DROP TABLE IF EXISTS StatusReport;
 DROP TABLE IF EXISTS CurrentPopulation;
-DROP TABLE IF EXISTS Locations;
-DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Location;
 
 -- List of locations shown in the app
-CREATE TABLE Locations (
-    IdNumber SERIAL PRIMARY KEY,
-    LocationName varchar(40),
-    MaxCapacity int,
+CREATE TABLE Location (
+    ID SERIAL PRIMARY KEY,
+    name varchar(40),
+    maxCapacity int,
     imageLocation varchar(50)
     );
 
 -- List of current status reports made by users
-CREATE TABLE CurrentStatus (
-    IdNumber SERIAL PRIMARY KEY,
-    ActivityStatus varchar(15),
-    LocationID int references Locations(IdNumber),
-    ReportedTime TIMESTAMP
+CREATE TABLE StatusReport (
+    ID SERIAL PRIMARY KEY,
+    status int,
+    locationID int references Location(ID),
+    reportedTime TIMESTAMP
     );
 
 -- List of actual card swipe number reports from dining halls
 CREATE TABLE CurrentPopulation (
-    IdNumber SERIAL PRIMARY KEY,
-    LocationID int references Locations(IdNumber),
-    -- We will come up with an algorithm to calculate estimated number of people currently at the dining hall
-    EstimatedPopulation int,
-    ReportedTime TIME,
-    ReportedDay varchar(10),
-    CardSwipeNumber int
+    ID SERIAL PRIMARY KEY,
+    locationID int references Location(ID),
+    estimatedPopulation int,
+    reportedTime TIME,
+    reportedDay varchar(10),
+    cardSwipeNumber int
     );
 
--- List of users registered to system
-CREATE TABLE Users (
-    UserId varchar(25) PRIMARY KEY,
-    UserPassword varchar(50),
-    UserType varchar(20)
-    );
 
 -- Allow users to select data from the tables.
-GRANT SELECT ON Locations TO PUBLIC;
-GRANT SELECT ON CurrentStatus TO PUBLIC;
+GRANT SELECT ON Location TO PUBLIC;
+GRANT SELECT ON StatusReport TO PUBLIC;
 GRANT SELECT ON CurrentPopulation TO PUBLIC;
-GRANT SELECT ON Users TO PUBLIC;
 
 -- Sample data to populate the tables
-INSERT INTO Locations (LocationName, MaxCapacity, imageLocation) VALUES
+INSERT INTO Location (name, maxCapacity, imageLocation) VALUES
     ('Commons Dining Hall', 500, '../assets/locations/commons.jpg'),
     ('Knollcrest Dining Hall', 400, '../assets/locations/knollcrest.jpg'),
     ('Uppercrust', 100, '../assets/locations/uppercrust.jpg'),
@@ -59,22 +50,16 @@ INSERT INTO Locations (LocationName, MaxCapacity, imageLocation) VALUES
     ('Peet''s', 50, '../assets/locations/peets.jpg')
     ;
 
-INSERT INTO Users (UserId, UserPassword, UserType) VALUES 
-    ('Id_1', 'Password_1', 'Dining Hall Faculty'),
-    ('Id_2', 'Password_2', 'Dining Hall Faculty'),
-    ('UserId_1', 'Password_3', 'Calvin User')
+
+INSERT INTO StatusReport( status, locationID, reportedTime) VALUES
+    (3,  1, '2020-10-16 13:30:00'),
+    (2, 2, '2020-10-16 14:20:00'),
+    (4, 3, '2020-10-16 15:10:00'),
+    (5,  1, '2020-10-16 13:40:00')
     ;
 
 
-INSERT INTO CurrentStatus( ActivityStatus, LocationID, ReportedTime) VALUES
-    ('Busy',  1, '2020-10-16 13:30:00'),
-    ('Slightly Busy', 2, '2020-10-16 14:20:00'),
-    ('Very Busy', 3, '2020-10-16 15:10:00'),
-    ('Busy',  1, '2020-10-16 13:40:00')
-    ;
-
-
-INSERT INTO CurrentPopulation(LocationId, EstimatedPopulation, ReportedTime, ReportedDay, CardSwipeNumber) VALUES
+INSERT INTO CurrentPopulation(locationID, estimatedPopulation, reportedTime, reportedDay, cardSwipeNumber) VALUES
     (1, 200, '13:30:00', 'Monday', 100),
     (1, 150, '13:45:00', 'Monday', 50),
     (2, 150,  '13:30:00', 'Monday', 60),
