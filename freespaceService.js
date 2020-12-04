ee725376eace33915f5765e5f837585e0ebbc5db
ZC <zc@ZCs-MacBook-Air.local>
2020-12-04 01:46:23 -0700

    Updated query for location and statusreport table join

--- freespaceService.js
+++ freespaceService.js
@@ -40,7 +40,7 @@ router.get("/currentpopulations/:id", readCurrentPopulation);
 router.get("/locationstatus", readLocationStatuses);
 
 // POST
-router.post('/currentstatus', createReport);
+router.post('/statusreports', createReport);
 
 app.use(allowCrossDomain);
 app.use(router);
@@ -129,10 +129,10 @@ function readCurrentPopulation(req, res, next) {
 
 
 function readLocationStatuses(req, res, next) {
-    db.many("SELECT LocationID, AVG(status), name FROM StatusReport, Location \
-                WHERE LocationID = Location.ID \
-                GROUP BY name, LocationID \
-                ORDER BY LocationID")
+    db.many("SELECT LocationID as key, LocationID, name, AVG(status) as statusAverage FROM StatusReport, Location \
+             WHERE LocationID = Location.ID \
+             GROUP BY name, LocationID \
+             ORDER BY LocationID")
         .then(data => {
             res.send(data);
         })
